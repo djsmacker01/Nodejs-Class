919f96cc8b96b0a7c9541f7334778d8571c3e0a8
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-08 19:40:16 +0000

    New commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const add = require('./add');
+const Person = require('./person')
 
-const sum = add (2, 3);
+const person1 = new Person()
 
-console.log("Hello world");
-console.log(sum);
+// const Person2 = new Person('Mary',75)
+
+person1.greeting('john', 30);
+
+// Person2.greeting();
+
+// const add = require('./add');
+
+// const sum = add (2, 3);
+
+// console.log("Hello world");
+// console.log(sum);
---
+++ person.js
@@ -0,0 +1,20 @@
+// Module wrapper
+// (function (exports, require, module, _filename, _dirname) {
+
+// })
+
+console.log(__filename, __dirname);
+
+class Person  {
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting(){
+        console.log(`My name is ${this.name} and i am ${this.age}`)
+    }
+}
+
+
+module.exports = Person;
