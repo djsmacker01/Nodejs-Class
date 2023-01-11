529f168c61c33e4712a771d24e99719eee7de8a4
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-11 12:45:04 +0000

    New Commit

---
+++ reference/OS_demo.js
@@ -0,0 +1,17 @@
+
+const OS = require('os');
+
+//platform 
+console.log(OS.platform());
+
+//Cpu Architecture
+ console.log(OS.arch());
+
+ //cpu core info
+ console.log(OS.cpus());
+
+ //Free memory info
+ console.log(OS.freemem());
+
+  //total memory info
+ console.log(OS.freemem());
